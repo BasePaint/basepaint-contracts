ed39b0bbd60ef50ab7b050635664b1f45663cbf6
Nader <alannaderdev@gmail.com>
2025-04-07 11:30:47 -0700

    upgrade sub contract to 1155

--- src/BasePaintSubscription.sol
+++ src/BasePaintSubscription.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 
-//  ____                  ____       _       _   
-// | __ )  __ _ ___  ___ |  _ \ __ _(_)_ __ | |_ 
+//  ____                  ____       _       _
+// | __ )  __ _ ___  ___ |  _ \ __ _(_)_ __ | |_
 // |  _ \ / _` / __|/ _ \| |_) / _` | | '_ \| __|
-// | |_) | (_| \__ \  __/|  __/ (_| | | | | | |_ 
+// | |_) | (_| \__ \  __/|  __/ (_| | | | | | |_
 // |____/ \__,_|___/\___||_|   \__,_|_|_| |_|\__|
-//  ____        _                  _       _   _             
-// / ___| _   _| |__  ___  ___ _ __(_)_ __ | |_(_) ___  _ __  
-// \___ \| | | | '_ \/ __|/ __| '__| | '_ \| __| |/ _ \| '_ \ 
+//  ____        _                  _       _   _
+// / ___| _   _| |__  ___  ___ _ __(_)_ __ | |_(_) ___  _ __
+// \___ \| | | | '_ \/ __|/ __| '__| | '_ \| __| |/ _ \| '_ \
 //  ___) | |_| | |_) \__ \ (__| |  | | |_) | |_| | (_) | | | |
 // |____/ \__,_|_.__/|___/\___|_|  |_| .__/ \__|_|\___/|_| |_|
 //                                   |_|
@@ -16,6 +16,7 @@ pragma solidity ^0.8.17;
 
 import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 import {IERC1155} from "openzeppelin-contracts/contracts/token/ERC1155/IERC1155.sol";
+import {ERC1155} from "openzeppelin-contracts/contracts/token/ERC1155/ERC1155.sol";
 
 interface IBasePaint is IERC1155 {
     function openEditionPrice() external view returns (uint256);
@@ -23,158 +24,86 @@ interface IBasePaint is IERC1155 {
     function today() external view returns (uint256);
 }
 
-contract BasePaintSubscription is Ownable {
-
-    event BalanceLessThanRequired(
-        address indexed _address, uint96 indexed _actualBalance, uint256 indexed _expectedBalance
-    );
-
-    event MintSkippedBalance(address indexed _address, uint96 indexed _balance, uint8 indexed _amountToMint);
-
-    event MintSkippedDaily(address indexed _address, uint96 indexed _balance, uint16 indexed _lastMinted);
-
-    event Subscribed(address indexed subscriber, address indexed mintTo, uint96 indexed balance, uint8 mintPerDay);
-
-    event SubscriptionExtended(address indexed subscriber);
-
-    event Unsubscribed(address indexed subscriber, uint256 refundAmount);
-
-    error CantBeZero();
-    error MustSendETH();
-    error NoBalance();
-    error NotEnoughMinted();
-    error NotMinter();
-    error RefundFailed();
-    error SubAlreadyExists();
-    error SubDoesntExist();
-    error WrongAmount();
-
+contract BasePaintSubscription is Ownable, ERC1155 {
     IBasePaint public basepaint;
-    address public minter;
 
-    struct Subscription {
-        address mintToAddress;
-        uint96 balance;
-        uint8 mintPerDay;
-        uint16 lastMinted;
-        bool active;
-    }
+    event Subscribed(address indexed subscriber, address indexed mintTo, uint256 length);
 
-    mapping(address => Subscription) public subscriptions;
+    error NotEnoughMinted();
+    error WrongEthAmount();
+    error InsufficientBalance();
 
-    constructor(address _basepaint, address _minter, address _owner) Ownable(_owner) {
+    constructor(address _basepaint, address _owner)
+        Ownable(_owner)
+        ERC1155("https://basepaint.xyz/api/subscription/{id}")
+    {
         basepaint = IBasePaint(_basepaint);
-        minter = _minter;
     }
 
     function subscribe(uint8 _mintPerDay, address _mintToAddress, uint256 _length) external payable {
-        uint256 value = msg.value;
-        if (value == 0) revert MustSendETH();
-        if (_mintPerDay == 0) revert CantBeZero();
-
-        address sender = msg.sender;
-
         uint256 price = basepaint.openEditionPrice();
         uint256 total = price * _mintPerDay * _length;
-        
-        if (value != total) revert WrongAmount();
 
-        Subscription storage sub = subscriptions[sender];
-        if (sub.active) revert SubAlreadyExists();
+        if (msg.value != total) revert WrongEthAmount();
 
-        address mintTo = _mintToAddress == address(0) ? sender : _mintToAddress;
+        emit Subscribed(msg.sender, _mintToAddress, _length);
 
-        sub.mintToAddress = mintTo;
-        sub.balance = uint96(value);
-        sub.mintPerDay = _mintPerDay;
-        sub.lastMinted = 0;
-        sub.active = true;
-
-        emit Subscribed(sender, mintTo, uint96(value), _mintPerDay);
-    }
-
-    function unsubscribe() external {
-        Subscription storage subscription = subscriptions[msg.sender];
-
-        if (!subscription.active) revert SubDoesntExist();
-        if (subscription.balance == 0) revert NoBalance();
-
-        uint256 refund = subscription.balance;
-        
-        subscription.active = false;
-        subscription.balance = 0;
-
-        emit Unsubscribed(msg.sender, refund);
-
-        (bool success,) = payable(msg.sender).call{value: refund}("");
-        if (!success) revert RefundFailed();
+        uint256 todayId = basepaint.today();
+        for (uint256 i = 0; i < _length; i++) {
+            _mint(msg.sender, todayId + i, _mintPerDay, "");
+        }
     }
 
     function mintDaily(address[] calldata _addresses, uint256 _toMint) external {
-        if (msg.sender != minter) revert NotMinter();
-
         uint256 today = basepaint.today() - 1;
         uint256 mintCost = basepaint.openEditionPrice();
 
-        basepaint.mint{value: mintCost * _toMint}(today, _toMint);
+        uint256 totalMintCost = 0;
+        uint256 minted = 0;
 
-        uint256 minted;
         for (uint256 i; i < _addresses.length;) {
-            Subscription storage subscription = subscriptions[_addresses[i]];
-
-            if (!subscription.active || subscription.balance == 0) {
-                emit MintSkippedBalance(_addresses[i], subscription.balance, subscription.mintPerDay);
-
+            uint256 tokenBalance = balanceOf(_addresses[i], today);
+            if (tokenBalance == 0) {
                 unchecked {
                     ++i;
                 }
                 continue;
             }
 
-            uint48 count = subscription.mintPerDay;
-            uint256 totalSpend = mintCost * count;
-
-            if (subscription.balance < totalSpend) {
-                emit BalanceLessThanRequired(_addresses[i], subscription.balance, totalSpend);
-
-                unchecked {
-                    ++i;
-                }
-                continue;
-            }
-
-            if (subscription.lastMinted == today) {
-                emit MintSkippedDaily(_addresses[i], subscription.balance, subscription.lastMinted);
-
-                unchecked {
-                    ++i;
-                }
-                continue;
-            }
-
-            subscription.lastMinted = uint16(today);
-            subscription.balance -= uint96(totalSpend);
-
-            basepaint.safeTransferFrom(address(this), subscription.mintToAddress, today, count, "");
-
+            totalMintCost += mintCost * tokenBalance;
+            minted += tokenBalance;
             unchecked {
-                minted += count;
                 ++i;
+            }
+        }
+
+        if (address(this).balance < totalMintCost) revert InsufficientBalance();
+
+        if (minted > 0) {
+            basepaint.mint{value: totalMintCost}(today, minted);
+
+            for (uint256 i; i < _addresses.length;) {
+                address mintToAddress = _addresses[i];
+
+                uint256 tokenBalance = balanceOf(_addresses[i], today);
+                if (tokenBalance > 0) {
+                    adminTransferAndBurn(_addresses[i], today, tokenBalance);
+
+                    basepaint.safeTransferFrom(address(this), mintToAddress, today, tokenBalance, "");
+                }
+
+                unchecked {
+                    ++i;
+                }
             }
         }
 
         if (minted != _toMint) revert NotEnoughMinted();
     }
 
-    function setNewMinter(address _newMinter) external onlyOwner {
-        minter = _newMinter;
-    }
-
-    receive() external payable {
-        Subscription storage subscription = subscriptions[msg.sender];
-        if (!subscription.active) revert SubDoesntExist();
-        subscription.balance += uint96(msg.value);
-        emit SubscriptionExtended(msg.sender);
+    function adminTransferAndBurn(address _from, uint256 _id, uint256 _amount) internal {
+        _safeTransferFrom(_from, address(this), _id, _amount, "");
+        _burn(address(this), _id, _amount);
     }
 
     function onERC1155Received(address, address, uint256, uint256, bytes memory) public pure returns (bytes4) {
